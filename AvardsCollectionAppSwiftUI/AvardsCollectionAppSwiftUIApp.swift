231053f8b58a55ba229ebeaf9f835cd3141950ae
Yurij Chekalyuk <yura_chekalyuk@mail.ru>
2021-06-18 22:38:22 +0300

    Initial Commit

---
+++ AvardsCollectionAppSwiftUI/AvardsCollectionAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvardsCollectionAppSwiftUIApp.swift
+//  AvardsCollectionAppSwiftUI
+//
+//  Created by SummeR on 18.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AvardsCollectionAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
